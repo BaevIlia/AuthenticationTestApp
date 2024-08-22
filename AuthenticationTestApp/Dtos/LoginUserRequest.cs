a8e08f70b4bc58623093aa6274be9ed31ceaf6aa
Ilia Baev <baevilia0809@gmail.com>
2024-08-22 14:13:34 +0300

    Созданы объекты DTO для работы с контроллерами

---
+++ AuthenticationTestApp/Dtos/LoginUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationTestApp.Dtos
+{
+    public record LoginUserRequest(
+        [Required] string Email,
+        [Required] string Password);
+    
+}
