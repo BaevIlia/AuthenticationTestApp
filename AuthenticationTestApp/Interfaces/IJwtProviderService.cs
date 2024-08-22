720844efe1b65a03b154af53e13fcfb4069268be
Ilia Baev <baevilia0809@gmail.com>
2024-08-22 14:29:27 +0300

    Реализован сервис генерации JWT токена

---
+++ AuthenticationTestApp/Interfaces/IJwtProviderService.cs
@@ -0,0 +1,9 @@
+﻿using AuthenticationTestApp.Database;
+
+namespace AuthenticationTestApp.Interfaces
+{
+    public interface IJwtProviderService
+    {
+        string GenerateToken(User user);
+    }
+}
---
+++ AuthenticationTestApp/Services/JwtProviderService.cs
@@ -0,0 +1,38 @@
+﻿using AuthenticationTestApp.Database;
+using AuthenticationTestApp.Options;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Claims;
+using AuthenticationTestApp.Interfaces;
+
+namespace AuthenticationTestApp.Services
+{
+    public class JwtProviderService : IJwtProviderService
+    {
+        private readonly JwtOptions _options;
+        public JwtProviderService(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public string GenerateToken(User user) 
+        {
+            Claim[] claims = [new("userId", user.Id.ToString())]; 
+            var signingCredentinals = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: signingCredentinals,
+                expires: DateTime.UtcNow.AddHours(_options.ExpitesHours)
+                );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}
