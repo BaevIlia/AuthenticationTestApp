7781785690b1613e2e7bf5c3fc64d8a4fb50ef31
Ilia Baev <baevilia0809@gmail.com>
2024-08-22 13:51:18 +0300

    Добавленны нереализованные методы register и login в контроллер

--- AuthenticationTestApp/Controllers/UserController.cs
+++ AuthenticationTestApp/Controllers/UserController.cs
@@ -6,6 +6,15 @@ namespace AuthenticationTestApp.Controllers
     [Route("/user")]
     public class UserController : ControllerBase
     {
-        
+        [HttpPost("/register")]
+        public async Task<IActionResult> Register() 
+        {
+            return Ok();
+        }
+        [HttpPost("/login")]
+        public async Task<IActionResult> Login() 
+        {
+            return Ok();
+        }
     }
 }
