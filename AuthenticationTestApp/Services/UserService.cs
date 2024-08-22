cce4ad20e1e6cc10c1ce33692d7ad49aec1b0c71
Ilia Baev <baevilia0809@gmail.com>
2024-08-22 14:09:45 +0300

    Реализован метод регистрации , частично реализован метод логина

--- AuthenticationTestApp/Services/UserService.cs
+++ AuthenticationTestApp/Services/UserService.cs
@@ -1,6 +1,35 @@
-﻿namespace AuthenticationTestApp.Services
+﻿using AuthenticationTestApp.Database;
+using AuthenticationTestApp.Interfaces;
+
+namespace AuthenticationTestApp.Services
 {
     public class UserService
     {
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly IUserRepository _repository;
+        public UserService(IUserRepository repository, IPasswordHasher passwordHasher)
+        {
+            _repository = repository;
+            _passwordHasher = passwordHasher;
+        }
+        public async Task Register(string userName, string email, string password) 
+        {
+            var hasgedPassword = _passwordHasher.Generate(password);
+
+            var user = User.Create(Guid.NewGuid(), userName, hasgedPassword, email);
+
+            await _repository.Add(user);
+        }
+
+        public async Task<string> Login(string email, string password) 
+        {
+            var user = await _repository.GetByEmail(email);
+
+            var result = _passwordHasher.Verify(password, user.PasswordHash);
+
+            if (!result)
+                throw new Exception("Failed to login");
+            
+        }
     }
 }
