2d4b48e0b85f00cfce7412bfa1915582b125d6c2
Ilia Baev <baevilia0809@gmail.com>
2024-08-23 14:12:39 +0300

    Создана конфигурация пермиссий

---
+++ AuthenticationTestApp/Database/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using AuthenticationTestApp.Database.Entities;
+using AuthenticationTestApp.Database.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthenticationTestApp.Database.Configurations
+{
+    public class PermissionConfiguration : IEntityTypeConfiguration<PermissionEntity>
+    {
+        public void Configure(EntityTypeBuilder<PermissionEntity> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            var permissions = Enum
+                .GetValues<Permission>()
+                .Select(p => new PermissionEntity
+                {
+                    Id = (int)p,
+                    Name = p.ToString()
+                });
+
+            builder.HasData(permissions);
+        }
+    }
+}
